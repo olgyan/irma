d790fb98a4181e34041448ddd1bcdc4d1f2eface
olgyan <natsukusa@rambler.ru>
2025-06-09 14:04:02 +0300

    Update files_together.py

--- files_together.py
+++ files_together.py
@@ -1,4 +1,3 @@
-import os
 import sys
 from pathlib import Path
 
@@ -6,19 +5,22 @@ from tqdm import tqdm
 
 from . import get_text
 
-def files_together(folder_name: os.PathLike):
+def files_together(folder_name):
     folder = Path(folder_name)
-    assert folder.is_dir(), 'folder_name must be a valid directory'
+    assert folder.is_dir(), 'нет такой папки'
     
     txt_files = [f for f in folder.iterdir() if f.suffix == '.txt']
-    assert txt_files, 'Directory must contain .txt files'
+    assert txt_files, 'Папка должна соджержать файлы .txt'
     
     records = list(
         get_text.outof(file, 'to string')
-        for file in tqdm(txt_files, desc="Processing files")
+        for file in tqdm(txt_files, desc="Собираем файлы воедино")
     )
     get_text.into(f'{folder_name}.txt', 'from_records', records)
 
 if __name__ == "__main__":
     if len(sys.argv) > 0:
         files_together(sys.argv[1])
+    else:
+        folder_name = input('Папка с файлами, которые надо собрать: ')
+        files_together(folder_name)
